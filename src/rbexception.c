af21db997638bce202d352f0a91ee1ec041075f6
Jarek 'Jarcec' Cecho <jaroslav.cecho@avg.com>
2009-09-22 07:58:53 +0200

    Jarcec is stupid
    Add missing files :-$

---
+++ src/rbexception.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "clips/clips.h"
+#include "rbclips.h"
+#include "rbexception.h"
+
+/* Definitions */
+VALUE cl_eException;
+VALUE cl_eArgError;
---
+++ src/rbexception.h
@@ -0,0 +1,10 @@
+#ifndef _RBEXCEPTION_H_
+#define _RBEXCEPTION_H_
+
+//! Clips::Excetion
+extern VALUE cl_eException;
+
+//! Clips::ArgumentError
+extern VALUE cl_eArgError;
+
+#endif // _RBEXCEPTION_H_
